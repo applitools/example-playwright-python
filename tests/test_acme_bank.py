474c4f32eb2743aee2a28fb8c790e17effd075d9
Pandy Knight <andylpk.247@gmail.com>
2023-03-02 15:18:06 -0500

    Implemented the test

--- tests/test_acme_bank.py
+++ tests/test_acme_bank.py
@@ -1,13 +1,14 @@
+from applitools.playwright import Eyes, Target
 from playwright.sync_api import Page
 
 
-def test_log_into_bank_account(page: Page) -> None:
+def test_log_into_bank_account(page: Page, eyes: Eyes) -> None:
 
   # Load the login page.
   page.goto("https://demo.applitools.com")
 
   # Verify the full login page loaded correctly.
-  # eyes.check(Target.window().fully().with_name("Login page"))
+  eyes.check(Target.window().fully().with_name("Login page"))
 
   # Perform login.
   page.locator('id=username').fill('andy')
@@ -16,4 +17,4 @@ def test_log_into_bank_account(page: Page) -> None:
 
   # Verify the full main page loaded correctly.
   # This snapshot uses LAYOUT match level to avoid differences in closing time text.
-  # eyes.check(Target.window().fully().with_name("Main page").layout())
+  eyes.check(Target.window().fully().with_name("Main page").layout())
